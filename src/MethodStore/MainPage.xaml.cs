5c9b824e732ac4525a01290cc03c6b016716c4d9
djserega <galyuk.sergey@gmail.com>
2018-06-23 15:44:52 +0300

    initial main menu

---
+++ src/MethodStore/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace MethodStore
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Models.Method> _listMethods = new ObservableCollection<Models.Method>();
+        public ObservableCollection<Models.Method> ListMethods { get => _listMethods; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            _listMethods.Add(new Models.Method() { Group = "<без группы>", Type = "Общий модуль" });
+            _listMethods.Add(new Models.Method() { Group = "<без группы>", Type = "Справочник" });
+
+            //DataGridMethods.ite
+        }
+    }
+}
---
+++ src/MethodStore/Models/Method.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodStore.Models
+{
+    public class Method
+    {
+        public Guid ID { get; set; }
+        public string Group { get; set; }
+        public string Type { get; set; }
+        public string ObjectName { get; set; }
+        public string MethodName { get; set; }
+
+        public string MethodInvokationString { get; set; }
+        public string Description { get; set; }
+
+        public bool TemplateAddToText { get; set; }
+        public string TemplateName { get; set; }
+        public string TemplateTextCorrect { get; set; }
+        public string TemplateAddToContextMenu { get; set; }
+    }
+}
