361bfa59327491b20b9ee76be951a19ee362f282
djserega <galyuk.sergey@gmail.com>
2018-06-23 16:23:17 +0300

    initialize page method

---
+++ src/MethodStore/PageMethod.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MethodStore
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class PageMethod : Page
+    {
+        private Models.Method _method = new Models.Method();
+        public Models.Method Method { get => _method; }
+
+        public PageMethod()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        private void PageMethodPage_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
