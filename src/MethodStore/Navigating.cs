a9848e9d97b8e78e99c7d3d3372e661f51a5e9bf
dj_serega <galyuk.sergey@gmail.com>
2018-06-25 21:12:54 +0300

    add static class navigating

---
+++ src/MethodStore/Navigating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MethodStore
+{
+    internal static class Navigating
+    {
+        internal static void Navigate(Type typePage, object param = null)
+        {
+            ParametersNavigating parameters = new ParametersNavigating()
+            {
+                parameters = param
+            };
+
+            Frame frame = Window.Current.Content as Frame;
+            frame.Navigate(typePage, parameters);
+        }
+    }
+}
