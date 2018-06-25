9627efe513f09b1048043cd921b4053fa4b12813
djserega <galyuk.sergey@gmail.com>
2018-06-25 14:13:36 +0300

    add usercontrol TextBoxButtonCreate

---
+++ src/MethodStore/TextBoxButtonCreate.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пользовательский элемент управления" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MethodStore
+{
+    internal delegate void Click();
+
+    public sealed partial class TextBoxButtonCreate : UserControl
+    {
+        internal event Click ClickNew;
+
+        public string Header { get; set; }
+        public string Text { get; set; }
+
+        public TextBoxButtonCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonNew_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClickNew == null)
+                return;
+
+            ClickNew();
+        }
+    }
+}
