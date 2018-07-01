01819ab949861a668abcf7d86931d7a9f46bbab0
dj_serega <galyuk.sergey@gmail.com>
2018-07-01 10:10:28 +0300

    added class RemovingText, enums

---
+++ src/MethodStore/Models/RemovingText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodStore.Models
+{
+    public class RemovingText
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public TypesRemovingText Type { get; set; }
+    }
+}
